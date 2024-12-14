3fe3c59df2fc32928f3608e87e9e31ba6c40b68d
Kautuk Dwivedi <kautukdwivedi@gmail.com>
2024-12-14 23:37:43 +0530

    Majority Element 1

---
+++ Arrays/MajorityElement1.java
@@ -0,0 +1,26 @@
+public class MajorityElement1 {
+    // Leetcode 11: https://leetcode.com/problems/container-with-most-water/
+
+    public static void main(String[] args) {
+        int[] nums = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(majorityElement(nums));
+    }
+
+    public static int majorityElement(int[] nums) {
+        int count = 0;
+        int ele = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                ele = num;
+                count = 1;
+            } else if (ele == num) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return ele;
+    }
+}
